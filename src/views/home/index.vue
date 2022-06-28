1ee25fada11d86e128beced33084b6d32eaa5a25
WaynePeng <pengwayne@outlook.com>
2022-06-29 02:39:50 +0800

    test

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { useLangue } from "@/store";
+import { useLangue } from '@/store'
 
-const langStore = useLangue();
+const langStore = useLangue()
 </script>
 
 <template>
   <div class="home">
     {{ langStore.lang }}
     <el-date-picker type="date" placeholder="Pick a day" />
-    <el-button> 小维v </el-button>
+    <el-button> 小维 </el-button>
   </div>
 </template>
 
